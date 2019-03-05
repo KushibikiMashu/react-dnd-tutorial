30085afd75c0f0a6f085234daa699b65fafd4269
KushibikiMashu <kushibiki3413@gmail.com>
2019-03-05 13:10:39 +0900

    ナイトの移動をL形のみに限定する

--- src/Board.js
+++ src/Board.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Square from './Square';
 import Knight from './Knight';
-import {moveKnight} from "./Game";
+import {moveKnight, canMoveKnight} from "./Game";
 
 function renderSquare(i, [knightX, knightY]) {
     const x = i % 8;
@@ -20,7 +20,9 @@ function renderSquare(i, [knightX, knightY]) {
 }
 
 function handleSquareClick(toX, toY) {
+  if (canMoveKnight(toX, toY)) {
     moveKnight(toX, toY);
+  }
 }
 
 function Board({knightPosition}) {
--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-let knightPosition = [0,0];
+let knightPosition = [1,7];
 let observer = null;
 
 // ナイトの位置の初期化と移動を知らせる
@@ -18,4 +18,13 @@ export function observe(o) {
 export function moveKnight(toX, toY) {
     knightPosition = [toX, toY];
     emitChange();
+}
+
+export function canMoveKnight(toX, toY) {
+  const [x, y] = knightPosition;
+  const dx = toX - x;
+  const dy = toY - y;
+
+  return (Math.abs(dx) === 2 && Math.abs(dy) === 1) ||
+         (Math.abs(dx) === 1 && Math.abs(dy) === 2);
 }
