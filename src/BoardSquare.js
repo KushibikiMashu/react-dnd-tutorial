0638063f1956865753b273aa2441482cfd5b9998
KushibikiMashu <kushibiki3413@gmail.com>
2019-03-05 20:40:42 +0900

    ドロップできるマスの色を表示する

--- src/BoardSquare.js
+++ src/BoardSquare.js
@@ -17,12 +17,28 @@ const squareTarget = {
 function collect(connect, monitor) {
     return {
         connectDropTarget: connect.dropTarget(),
-        isOver: monitor.isOver()
+        isOver: monitor.isOver(),
+        canDrop: monitor.canDrop()
     }
 }
 
-function BoardSquare({x, y, connectDropTarget, isOver, children}) {
+function BoardSquare({x, y, connectDropTarget, isOver, canDrop, children}) {
     const black = (x + y) % 2 === 1;
+
+    function renderOverlay(color){
+        return (
+            <div style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                height: '100%',
+                width: '100%',
+                zIndex: 1,
+                opacity: 0.5,
+                backgroundColor: color,
+            }} />
+        );
+    }
 
     return connectDropTarget(
         <div style={{
@@ -33,18 +49,9 @@ function BoardSquare({x, y, connectDropTarget, isOver, children}) {
             <Square black={black}>
                 {children}
             </Square>
-            {isOver &&
-            <div style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                height: '100%',
-                width: '100%',
-                zIndex: 1,
-                opacity: 0.5,
-                backgroundColor: 'yellow',
-            }}/>
-            }
+            {isOver && !canDrop && renderOverlay('red')}
+            {!isOver && canDrop && renderOverlay('yellow')}
+            {isOver && canDrop && renderOverlay('green')}
         </div>
     );
 }
